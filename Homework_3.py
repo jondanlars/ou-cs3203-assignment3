1323b63bcecad3f720a0238f6da029ff0205163b
Jonathan Larson <Jonathan.d.larson-1@ou.edu>
2023-09-16 16:23:57 -0500

    Added code to input a list and run functions

--- Homework_3.py
+++ Homework_3.py
@@ -11,3 +11,20 @@ def listProd(list):
     for i in range(len(list)):
         prod *= list[i];
     return prod;
+
+#main code
+list = [];
+i = 0;
+element = None;
+
+#Input the list
+print("Enter 5 integers for the list");
+while i < 5:
+    list.append(int(input()));
+    i += 1;
+
+#Print out the results
+print("List is:");
+print(list);
+print("Sum of elements is: " + str(listAdd(list)));
+print("Product of elements is: " + str(listProd(list)));
